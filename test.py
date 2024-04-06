01fb59cd4e30c74d7f9f4f9ef3957bd3e27a9f4d
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2024-04-06 20:32:58 +0530

    few changes

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from safer import CompanySnapshot
 
 client = CompanySnapshot()
-company = client.get_by_usdot_number(1590983)
-print(company.to_json())
+
+results = client.search('python')
+company = results[0].get_company_snapshot()
+print(company.legal_name)
